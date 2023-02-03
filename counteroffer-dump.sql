3b5f9459830c807ea0ea24124a984dfc0d70f352
Bob Stark <bob@datagotchi.net>
2023-02-02 16:31:33 -0800

    First sql dump

---
+++ counteroffer-dump.sql
@@ -0,0 +1,336 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.4
+-- Dumped by pg_dump version 14.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: experiences; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.experiences (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    title text NOT NULL,
+    company text NOT NULL,
+    startdate date NOT NULL,
+    enddate date,
+    summary text
+);
+
+
+ALTER TABLE public.experiences OWNER TO postgres;
+
+--
+-- Name: experiences_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.experiences_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.experiences_id_seq OWNER TO postgres;
+
+--
+-- Name: experiences_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.experiences_id_seq OWNED BY public.experiences.id;
+
+
+--
+-- Name: facts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.facts (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    key text NOT NULL,
+    value text NOT NULL
+);
+
+
+ALTER TABLE public.facts OWNER TO postgres;
+
+--
+-- Name: facts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.facts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.facts_id_seq OWNER TO postgres;
+
+--
+-- Name: facts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.facts_id_seq OWNED BY public.facts.id;
+
+
+--
+-- Name: messages; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.messages (
+    id integer,
+    type character varying,
+    value character varying,
+    job_id integer,
+    datetime date,
+    sender character varying
+);
+
+
+ALTER TABLE public.messages OWNER TO postgres;
+
+--
+-- Name: tags; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tags (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    experience_id integer NOT NULL,
+    value text NOT NULL
+);
+
+
+ALTER TABLE public.tags OWNER TO postgres;
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.tags_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tags_id_seq OWNER TO postgres;
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.tags_id_seq OWNED BY public.tags.id;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer DEFAULT nextval('public.users_id_seq'::regclass) NOT NULL,
+    username character varying NOT NULL,
+    hashed_password character varying NOT NULL,
+    current_session character varying
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: experiences id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.experiences ALTER COLUMN id SET DEFAULT nextval('public.experiences_id_seq'::regclass);
+
+
+--
+-- Name: facts id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.facts ALTER COLUMN id SET DEFAULT nextval('public.facts_id_seq'::regclass);
+
+
+--
+-- Name: tags id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tags ALTER COLUMN id SET DEFAULT nextval('public.tags_id_seq'::regclass);
+
+
+--
+-- Data for Name: experiences; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.experiences (id, user_id, title, company, startdate, enddate, summary) FROM stdin;
+1	1	Founder	Datagotchi Labs	2018-01-01	\N	R&D on data-driven UIs to solve wicked social problems, including the job market and news reliability
+2	1	Staff R&D Engineer	Tanium	2020-06-01	2022-06-01	Researched ways to improve user experiences using data, AI, ML, etc.
+\.
+
+
+--
+-- Data for Name: facts; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.facts (id, user_id, key, value) FROM stdin;
+1	1	Job Search Stage	Passively open to new opportunities
+2	1	Objective	To apply my passion in human-machine collaboration (the UX of AI), utilizing my skills in full-stack engineering, machine learning, data visualization, UX, and team leadership
+\.
+
+
+--
+-- Data for Name: messages; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.messages (id, type, value, job_id, datetime, sender) FROM stdin;
+\.
+
+
+--
+-- Data for Name: tags; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.tags (id, user_id, experience_id, value) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, username, hashed_password, current_session) FROM stdin;
+1	Bob Stark	2a9d119df47ff993b662a8ef36f9ea20	4c393d3e-288b-4155-a9fd-d77b58c49547
+\.
+
+
+--
+-- Name: experiences_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.experiences_id_seq', 2, true);
+
+
+--
+-- Name: facts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.facts_id_seq', 2, true);
+
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.tags_id_seq', 1, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- Name: experiences experiences_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.experiences
+    ADD CONSTRAINT experiences_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: facts facts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.facts
+    ADD CONSTRAINT facts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tags tags_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tags
+    ADD CONSTRAINT tags_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_username UNIQUE (username);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ix_experiences_user_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_experiences_user_id ON public.experiences USING btree (user_id);
+
+
+--
+-- Name: ix_facts_user_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_facts_user_id ON public.facts USING btree (user_id);
+
+
+--
+-- Name: ix_users_username; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_users_username ON public.users USING btree (username);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
