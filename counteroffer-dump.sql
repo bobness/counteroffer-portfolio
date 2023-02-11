d74bb4fc3ff3d58eb8873e2e3888cd6b7a65dec0
Bob Stark <bob@datagotchi.net>
2023-02-10 17:16:06 -0800

    Updated sql dump to enable survey responses

--- counteroffer-dump.sql
+++ counteroffer-dump.sql
@@ -67,7 +67,8 @@ CREATE TABLE public.facts (
     id integer NOT NULL,
     user_id integer NOT NULL,
     key text NOT NULL,
-    value text NOT NULL
+    value text NOT NULL,
+    job_id integer
 );
 
 
@@ -96,20 +97,116 @@ ALTER SEQUENCE public.facts_id_seq OWNED BY public.facts.id;
 
 
 --
+-- Name: jobs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.jobs (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    email text NOT NULL,
+    company text,
+    archived boolean DEFAULT false NOT NULL
+);
+
+
+ALTER TABLE public.jobs OWNER TO postgres;
+
+--
+-- Name: jobs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.jobs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.jobs_id_seq OWNER TO postgres;
+
+--
+-- Name: jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.jobs_id_seq OWNED BY public.jobs.id;
+
+
+--
 -- Name: messages; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.messages (
-    id integer,
-    type character varying,
-    value character varying,
+    id integer NOT NULL,
+    value text,
+    question_id integer NOT NULL,
+    sender text,
     job_id integer,
-    datetime date,
-    sender character varying
+    datetime timestamp without time zone
 );
 
 
 ALTER TABLE public.messages OWNER TO postgres;
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.messages_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.messages_id_seq OWNER TO postgres;
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.messages_id_seq OWNED BY public.messages.id;
+
+
+--
+-- Name: questions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.questions (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    question character varying(255),
+    type character varying(20),
+    required boolean DEFAULT true NOT NULL
+);
+
+
+ALTER TABLE public.questions OWNER TO postgres;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.questions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.questions_id_seq OWNER TO postgres;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.questions_id_seq OWNED BY public.questions.id;
+
 
 --
 -- Name: tags; Type: TABLE; Schema: public; Owner: postgres
@@ -166,7 +263,7 @@ ALTER TABLE public.users_id_seq OWNER TO postgres;
 
 CREATE TABLE public.users (
     id integer DEFAULT nextval('public.users_id_seq'::regclass) NOT NULL,
-    username character varying NOT NULL,
+    email character varying NOT NULL,
     hashed_password character varying NOT NULL,
     current_session character varying
 );
@@ -186,6 +283,27 @@ ALTER TABLE ONLY public.experiences ALTER COLUMN id SET DEFAULT nextval('public.
 --
 
 ALTER TABLE ONLY public.facts ALTER COLUMN id SET DEFAULT nextval('public.facts_id_seq'::regclass);
+
+
+--
+-- Name: jobs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.jobs ALTER COLUMN id SET DEFAULT nextval('public.jobs_id_seq'::regclass);
+
+
+--
+-- Name: messages id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages ALTER COLUMN id SET DEFAULT nextval('public.messages_id_seq'::regclass);
+
+
+--
+-- Name: questions id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.questions ALTER COLUMN id SET DEFAULT nextval('public.questions_id_seq'::regclass);
 
 
 --
@@ -209,9 +327,17 @@ COPY public.experiences (id, user_id, title, company, startdate, enddate, summar
 -- Data for Name: facts; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.facts (id, user_id, key, value) FROM stdin;
-1	1	Job Search Stage	Passively open to new opportunities
-2	1	Objective	To apply my passion in human-machine collaboration (the UX of AI), utilizing my skills in full-stack engineering, machine learning, data visualization, UX, and team leadership
+COPY public.facts (id, user_id, key, value, job_id) FROM stdin;
+1	1	Job Search Stage	Passively open to new opportunities	\N
+2	1	Objective	To apply my passion in human-machine collaboration (the UX of AI), utilizing my skills in full-stack engineering, machine learning, data visualization, UX, and team leadership	\N
+\.
+
+
+--
+-- Data for Name: jobs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.jobs (id, user_id, email, company, archived) FROM stdin;
 \.
 
 
@@ -219,7 +345,18 @@ COPY public.facts (id, user_id, key, value) FROM stdin;
 -- Data for Name: messages; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.messages (id, type, value, job_id, datetime, sender) FROM stdin;
+COPY public.messages (id, value, question_id, sender, job_id, datetime) FROM stdin;
+\.
+
+
+--
+-- Data for Name: questions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.questions (id, user_id, question, type, required) FROM stdin;
+1	1	Which of my skills are most useful for the opportunity?	skills	t
+3	1	Other details about the opportunity	textarea	f
+2	1	What other skills are required/desired for the opportunity?	textarea	f
 \.
 
 
@@ -228,6 +365,16 @@ COPY public.messages (id, type, value, job_id, datetime, sender) FROM stdin;
 --
 
 COPY public.tags (id, experience_id, value) FROM stdin;
+1	1	Javascript
+2	1	Typescript
+3	1	NodeJS
+4	1	React
+5	2	Javascript
+6	2	Typescript
+7	2	React
+8	1	React Native
+9	2	NodeJS
+10	2	Go
 \.
 
 
@@ -235,8 +382,8 @@ COPY public.tags (id, experience_id, value) FROM stdin;
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, username, hashed_password, current_session) FROM stdin;
-1	Bob Stark	2a9d119df47ff993b662a8ef36f9ea20	4c393d3e-288b-4155-a9fd-d77b58c49547
+COPY public.users (id, email, hashed_password, current_session) FROM stdin;
+1	bob@datagotchi.net	2a9d119df47ff993b662a8ef36f9ea20	4c393d3e-288b-4155-a9fd-d77b58c49547
 \.
 
 
@@ -255,10 +402,31 @@ SELECT pg_catalog.setval('public.facts_id_seq', 2, true);
 
 
 --
+-- Name: jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.jobs_id_seq', 12, true);
+
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.messages_id_seq', 24, true);
+
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.questions_id_seq', 3, true);
+
+
+--
 -- Name: tags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.tags_id_seq', 1, false);
+SELECT pg_catalog.setval('public.tags_id_seq', 10, true);
 
 
 --
@@ -285,6 +453,30 @@ ALTER TABLE ONLY public.facts
 
 
 --
+-- Name: jobs jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.jobs
+    ADD CONSTRAINT jobs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: messages messages_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: questions questions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT questions_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: tags tags_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -293,11 +485,19 @@ ALTER TABLE ONLY public.tags
 
 
 --
+-- Name: jobs u_user_id_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.jobs
+    ADD CONSTRAINT u_user_id_email UNIQUE (user_id, email);
+
+
+--
 -- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
-    ADD CONSTRAINT unique_username UNIQUE (username);
+    ADD CONSTRAINT unique_username UNIQUE (email);
 
 
 --
@@ -323,10 +523,31 @@ CREATE INDEX ix_facts_user_id ON public.facts USING btree (user_id);
 
 
 --
+-- Name: ix_jobs_user_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_jobs_user_id ON public.jobs USING btree (user_id);
+
+
+--
+-- Name: ix_messages_question_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_messages_question_id ON public.messages USING btree (question_id);
+
+
+--
+-- Name: ix_questions_user_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_questions_user_id ON public.questions USING btree (user_id);
+
+
+--
 -- Name: ix_users_username; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX ix_users_username ON public.users USING btree (username);
+CREATE INDEX ix_users_username ON public.users USING btree (email);
 
 
 --
